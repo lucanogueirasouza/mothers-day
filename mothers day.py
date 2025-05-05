d27093a13a57aac267eb61d2e1ae296165ced250
Luca Nogueira de Souza <lucanogueira.desouza@gmail.com>
2025-05-05 14:35:09 -0300

    updated mothers day - first commit

---
+++ mothers day.py
@@ -0,0 +1,39 @@
+from time import sleep 
+
+mensagem = [
+    "\033[31mFeliz Dias das Mães\033[m",
+    "❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️",
+    "\033[32mMãe, obrigado por tudo que você faz por mim.\033[m",
+    "\033[33mVocê é especial, forte e cheia de amor!\033[m",
+    "\033[34mTe amo hoje e sempre!\033[m",
+    "❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️❤️"
+]
+
+for linha in mensagem: 
+    print (linha)
+    sleep(1)
+
+print ( "\033[31m    **      **      \033[m")
+sleep(1)
+print ( "  ******  ******     ")
+sleep(1)
+print ( "\033[31m ****************\033[m")
+sleep(1)
+print ( "******************")
+sleep(1)
+print ("\033[31m ****************\033[m")
+sleep(1)
+print ("  **************")
+sleep(1)
+print ("\033[31m   ************\033[m")
+sleep(1)
+print ("    **********")
+sleep(1)
+print ("\033[31m     ********\033[m")
+sleep(1)
+print ("      ******")
+sleep(1)
+print ("\033[31m       ****\033[m")
+sleep(1)
+print ("        **")
+
